5484ee6cac05ee13fe5c8038934fe151a55767b2
Benjamín Vicente <benjavicente@uc.cl>
2023-05-08 17:57:26 -0400

    offline

--- pwa-grupo-01/sw.js
+++ pwa-grupo-01/sw.js
@@ -3,7 +3,20 @@
 import { getMessaging, onBackgroundMessage } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-messaging-sw.js";
 import { app } from "./js/firebase/app.js";
 
-const version = "0.0.9";
+const version = "0.1.0";
+
+const staticFiles = [
+  "/pwa-grupo-01/",
+  "/pwa-grupo-01/index.html",
+  "/pwa-grupo-01/style.css",
+  "/pwa-grupo-01/main.js",
+  "/pwa-grupo-01/js/ui.js",
+  "/pwa-grupo-01/js/utils.js",
+  "/pwa-grupo-01/js/firebase/app.js",
+  "/pwa-grupo-01/js/firebase/auth.js",
+  "/pwa-grupo-01/js/firebase/db.js",
+  "/pwa-grupo-01/js/firebase/messaging.js",
+];
 
 export const messaging = getMessaging(app);
 
@@ -15,21 +28,55 @@ onBackgroundMessage(messaging, (payload) => {
   self.registration.showNotification(notificationTitle);
 });
 
-self.addEventListener("install", (event) => {
+addEventListener("install", (event) => {
   console.log(`[${version}] Service Worker installing...`);
+  event.waitUntil(
+    caches.open("pwagag-cache-" + version).then((cache) => {
+      cache.addAll(staticFiles);
+    })
+  );
 });
 
-self.addEventListener("activate", (event) => {
+addEventListener("activate", (event) => {
   console.log("Service Worker activating.");
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName.startsWith("pwagag-cache-") && cacheName !== "pwagag-cache-" + version)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
 });
 
-self.addEventListener("fetch", (event) => {
+addEventListener("fetch", (event) => {
   console.log("Service Worker fetching.");
-});
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // If the request is in the cache, return it
+      if (response) return response;
 
-self.onmessage = (event) => {
-  console.log("Service Worker received message:", event.data);
-};
+      // TODO: cache de otras cosas
+
+      // Esto de abajo no se usa pq cachea todo
+      // // If the request is not in the cache, fetch it
+      // return fetch(event.request).then((response) => {
+      //   // Clone the response to add it to the cache
+      //   const clonedResponse = response.clone();
+
+      //   caches.open("my-cache").then((cache) => {
+      //     // Add the response to the cache
+      //     cache.put(event.request, clonedResponse);
+      //   });
+
+      //   return response;
+      // });
+
+      return fetch(event.request);
+    })
+  );
+});
 
 const versionBroadcastChannel = new BroadcastChannel("version");
 versionBroadcastChannel.onmessage = (event) => {
